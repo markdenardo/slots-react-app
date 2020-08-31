e967ceb58b1ab4109b62f51a3db24c60bb34a47b
markdenardo <mark.denardo@gmail.com>
2020-08-31 17:49:52 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const cardArr = ['🂠', '🂡', '🂢', '🂣', '🂤', '🂥', '🂦', '🂧', '🂸', '🂩', '🂪', '🂫', '🂬', '🂭', '🂮', '🂱', '🂲', '🂳', '🂴', '🂵', '🂷', '🂸', '🂹', '🂺', '🂻', '🂼', '🂼', '🃁', '🃂', '🃃', '🃄', '🃅', '🃆', '🃇', '🃉', '🃊', '🃋', '🃌', '🃍', '🃎', '🃑', '🃒', '🃓', '🃔', '🃕', '🃖', '🃗', '🃘', '🃙', '🃚', '🃛', '🃜', '🃝', '🃞', '🃟'];
+ 
+  const [cards, setCards] = useState([
+    ['a','b','c']
+  ]);
+
+  const getRandomInt = max => {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  const buttonDown = () => {
+    for(let i=0; i<3; i++){
+      getRandomCard();
+      return <h1>randomCard</h1>
+    }
+    
+
+  }
+
+  const getRandomCard = () => {
+    let randomCard = cardArr[getRandomInt(cardArr.length)];
+    console.log("clicked");
+    console.log(randomCard);
+    return <h2>{randomCard}</h2>
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        SLOTS
+        <h1>
+          {cards}
+        </h1>
+        <span>
+          <button onClick = {buttonDown}>start spinning</button>
+        </span>  
       </header>
+      
     </div>
   );
 }
